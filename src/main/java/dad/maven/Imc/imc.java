e07d752b89d9329d8a711d2b63ef16dc96532ea7
AdrianNietoRamos <71700460+AdrianNietoRamos@users.noreply.github.com>
2020-10-17 10:10:11 +0100

    Update imc.java

--- src/main/java/dad/maven/Imc/imc.java
+++ src/main/java/dad/maven/Imc/imc.java
@@ -25,36 +25,49 @@ Text text2 = new Text("Altura");
 final TextField Altura = new TextField("0");
 Altura.setPrefColumnCount(4);
 final Text TextResul = new Text("");
+Text text3 = new Text("kg");
+Text text4 = new Text("cm");
+final Text resul = new Text("");
+
+
 
 HBox PesoBox = new HBox();
 PesoBox.setAlignment(Pos.BASELINE_CENTER);
 PesoBox.setSpacing(5);
-PesoBox.getChildren().addAll(text, Peso);
+PesoBox.getChildren().addAll(text, Peso, text3);
 
-HBox Alturabox = new HBox();
-Alturabox.setAlignment(Pos.BASELINE_CENTER);
-Alturabox.setSpacing(5);
-Alturabox.getChildren().addAll(text2, Altura);
+HBox AlturaBox = new HBox();
+AlturaBox.setAlignment(Pos.BASELINE_CENTER);
+AlturaBox.setSpacing(5);
+AlturaBox.getChildren().addAll(text2, Altura, text4);
 Text IMC = new Text("");
 Button Calcular = new Button("Calcular");
 Calcular.setOnAction(new EventHandler<ActionEvent>() {
+	
 public void handle(ActionEvent event) {
+
+	
 double peso = Integer.parseInt(Peso.getText());
 double altura = Integer.parseInt(Altura.getText());
 double op = peso / (altura * altura);
 double resultado = op * 10000;
-imc.setText("IMC: " + resultado);
+
+resul.setText("IMC: " + resultado);
 if (resultado < 18.5) {
 TextResul.setText("Bajo Peso");
+
 }
 if (resultado > 18.5 && resultado < 25) {
 TextResul.setText("Peso Normal");
+
 }
 if (resultado > 25 && resultado < 30) {
 TextResul.setText("Sobrepeso");
+
 }
 if (resultado > 30) {
 TextResul.setText("Obeso");
+
 }
 }
 
@@ -64,7 +77,7 @@ VBox root = new VBox();
 root.setSpacing(5);
 root.setAlignment(Pos.CENTER);
 
-root.getChildren().addAll(PesoBox, Alturabox, Calcular, IMC, TextResul);
+root.getChildren().addAll(PesoBox, AlturaBox, Calcular, IMC, resul ,TextResul);
 Scene scene = new Scene(root, 400, 400);
 primaryStage.setTitle("IMC");
 primaryStage.setScene(scene);
@@ -75,7 +88,7 @@ e.printStackTrace();
 }
 
 protected static void setText(String string) {
-	// TODO Auto-generated method stub
+	
 	
 }
 
